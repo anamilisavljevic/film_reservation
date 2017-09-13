1765e730d8361ee79eb370ac7471380c69421a23
Ana Milisavljevic <a.milisavljevic@msgnetconomy.com>
2017-09-13 23:02:43 +0200

    Added test for database

--- test/clj/film_reservation/test/db/core.clj
+++ test/clj/film_reservation/test/db/core.clj
@@ -5,7 +5,6 @@
             [clojure.java.jdbc :as jdbc]
             [film-reservation.config :refer [env]]
             [mount.core :as mount]))
-
 (use-fixtures
   :once
   (fn [f]
@@ -14,23 +13,28 @@
       #'film-reservation.db.core/*db*)
     (migrations/migrate ["migrate"] (select-keys env [:database-url]))
     (f)))
-
-(deftest test-users
+(deftest test-messages
   (jdbc/with-db-transaction [t-conn *db*]
-    (jdbc/db-set-rollback-only! t-conn)
-    (is (= 1 (db/create-user!
-               t-conn
-               {:id         "1"
-                :first_name "Sam"
-                :last_name  "Smith"
-                :email      "sam.smith@example.com"
-                :pass       "pass"})))
-    (is (= {:id         "1"
-            :first_name "Sam"
-            :last_name  "Smith"
-            :email      "sam.smith@example.com"
-            :pass       "pass"
-            :admin      nil
-            :last_login nil
-            :is_active  nil}
-           (db/get-user t-conn {:id "1"})))))
+                            (jdbc/db-set-rollback-only! t-conn)
+                            (let [timestamp (org.joda.time.DateTime.)]
+                              (is (= 1 (db/save-reservation!
+                                         t-conn
+                                         {:first_name "Tamara"
+                                          :last_name "Radevic"
+                                          :email "taca93@yahoo.com"
+                                          :date timestamp
+                                          :number_of_seat 12
+                                          :cinema "Usce"
+                                          :film  "Dunkirk"}
+                                         {:connection t-conn})))
+                              (is (=
+                                    {:first_name "Tamara"
+                                     :last_name "Radevic"
+                                     :email "taca93@yahoo.com"
+                                     :date  timestamp
+                                     :number_of_seat  12
+                                     :cinema "Usce"
+                                     :film  "Dunkirk"}
+                                    (-> (db/get-reservations t-conn {})
+                                        (first)
+                                        (select-keys [:first_name :last_name :email :date :number_of_seat :cinema :film])))))))
